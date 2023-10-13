f34da940b49d10f08ecc800508085865d379529d
Irena Vankova <irena.vanek@gmail.com>
2023-10-13 13:07:08 -0600

    var_cell list dims

--- iv_configs/chic_outputs/mpasHRES_to_fris.py
+++ iv_configs/chic_outputs/mpasHRES_to_fris.py
@@ -81,29 +81,30 @@ dsOut = xarray.Dataset()
 dsOut_cell = xarray.Dataset()
 dsOut_edge = xarray.Dataset()
 
-# dsOut[in_var_name] = ds[in_var_name].isel(nVertLevels=0, Time=0) # if want only specific dimension
 ds_mesh = xarray.open_dataset(inGridFileName)
 maxLevelCell = ds_mesh.maxLevelCell - 1
 for var in var_cell:
     print(var)
-    #if var in ['kineticEnergyCell']:
-    #dsOut_cell[var] = ds[var].isel(nVertLevels=[1, maxLevelCell])
-    #dsOut_cell[var] = ds[var].isel(nVertLevels=maxLevelCell)
-    dsOut_cell[f'{var}_top'] = ds[var].isel(nVertLevels=1)
-    dsOut_cell[f'{var}_bot'] = ds[var].isel(nVertLevels=maxLevelCell)
-    #t_floor = ds['temperature'].isel(nVertLevels=maxLevelCell)
+    dd = list(ds[var].dims)
+    if 'nVertLevels' in dd:
+        dsOut_cell[f'{var}_top'] = ds[var].isel(nVertLevels=1)
+        dsOut_cell[f'{var}_bot'] = ds[var].isel(nVertLevels=maxLevelCell)
+    else:
+        dsOut_cell[var] = ds[var].isel
 var_cell_out = list(dsOut_cell.keys())
 for var in var_edge:
     print(var)
-    if var in ['gmKappaScaling']:
+    dd = list(ds[var].dims)
+    if 'nVertLevelsP1' in dd:
         dsOut_edge[var] = ds[var].isel(nVertLevelsP1=0)
     else:
         dsOut_edge[var] = ds[var]
 var_edge_out = list(dsOut_edge.keys())
+
 print('remapping with python remapping')
 dsOut_cell = remapper.remap(dsOut_cell)
 dsOut_edge = remapper_edge.remap(dsOut_edge)
-#dsOut.merge([dsOut_cell, dsOut_edge])
+
 for var in var_cell_out:
     print(var)
     dsOut[var] = dsOut_cell[var]
